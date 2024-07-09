33933ea2227324ce599315cc297bb0d066b03c25
Qurcoo <qurcikidzegiorgi21@gmail.com>
2024-07-09 11:53:47 +0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -79,7 +79,7 @@ def post():
 @app.route("/posts", methods=["POST","GET"])
 def uplPosts1():
     posts = Uplposts.query.all()
-    role = User.query.get(role)
+    role = User.query.get(User.role=role)
     return render_template("posts.html", posts=posts, role=role)
 
 @app.route("/delete/<int:user_id>")
