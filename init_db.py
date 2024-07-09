102aa5b59a0187ceb7f0b6d8e4be11a1fa5d6ea2
Qurcoo <qurcikidzegiorgi21@gmail.com>
2024-07-09 09:54:24 +0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -3,3 +3,6 @@ from models import Product, Uplposts
 
 with app.app_context():
     db.create_all()
+    admin_user = User("admin", "password321","admin")
+    db.session.add(admin_user)
+    db.session.commit()
